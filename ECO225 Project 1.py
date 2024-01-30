2b7a4b0e08d74ca7963dd240ba026c87a1f3a098
Yuguang(William) Duan <157653044+william03duan@users.noreply.github.com>
2024-01-30 18:56:34 -0500

    Add files via upload
    Some rough work

---
+++ ECO225 Project 1.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # "project title" (finish this part at the very end of this project)
+
+# ## Outline
+# - Introduction
+# - Data Cleaning and Variables intepretation
+# - Summary Statistics table
+# - Figures and charts
+# - Conclusions
+# - References
+
+# ## Introduction
+
+# In recent years, global warming is widely concerned, and this phenomenon is becoming obvious in some ares, such as the North pole. 
+
+# 
+
+# ## Data Cleaning and Variables intepretation
+
+# The dependent variable (Y variable) is "Biodiversity", and 
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[17]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# At the beginning, we need to import the necessary python databases (pandas, numpy, and matplotlib) and load the "GlobalTemperatures" and "dataset with dataframe "df" shown below. 
+
+# In[24]:
+
+
+df = pd.read_csv('GlobalTemperatures.csv')
+df
+
+
+# Now, we are going to drop the empty value (null value) in the original table as below: 
+
+# In[25]:
+
+
+df.dropna()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
